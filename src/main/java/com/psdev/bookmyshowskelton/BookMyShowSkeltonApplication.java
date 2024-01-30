fdd1730ded3efd7ee76e2d8c17fd54a71693b6d9
premsagars.1811@gmail.com <premsagars.1811@gmail.com>
2024-01-30 23:10:12 +0530

    first commit

---
+++ src/main/java/com/psdev/bookmyshowskelton/BookMyShowSkeltonApplication.java
@@ -0,0 +1,13 @@
+package com.psdev.bookmyshowskelton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowSkeltonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowSkeltonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psdev/bookmyshowskelton/BookMyShowSkeltonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psdev.bookmyshowskelton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowSkeltonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
